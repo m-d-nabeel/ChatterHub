095fcdafff676ac580f8e9b6d35b1e808847d21e
m-d-nabeel <asharnabeel137@gmail.com>
2023-08-31 19:27:44 +0530

    preDeploy[0]

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,23 @@
 import { ModeToggle } from "@/components/mode-toggle";
-import { UserButton } from "@clerk/nextjs";
+import prismadb from "@/lib/db";
+import { UserButton, currentUser } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const user: User | null = await currentUser();
+  if (!user) {
+    return null;
+  }
+
+  const profile = await prismadb.profile.create({
+    data: {
+      userId: user.id,
+      name: user.firstName + " " + user.lastName,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
